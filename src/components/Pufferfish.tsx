5d371fa8df11b0378420014788dd832445d766f7
Khoa <khoa.h0086881@gmail.com>
2025-10-01 02:28:08 +0700

    Test sizing with md

--- src/components/Pufferfish.tsx
+++ src/components/Pufferfish.tsx
@@ -2,7 +2,7 @@ import Spline from "@splinetool/react-spline";
 
 export default function Pufferfish() {
   return (
-    <div className="md:w-full w-lg">
+    <div className="md:w-full w-md">
       <Spline scene="https://prod.spline.design/Eeq22nehzEKuTWxy/scene.splinecode" />
     </div>
   );
